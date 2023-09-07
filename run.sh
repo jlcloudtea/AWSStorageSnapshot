c8e51957fb0ab750d4628e1e3b0d0a903055e8a1
jlcloudtea <114410941+jlcloudtea@users.noreply.github.com>
2023-09-07 22:42:00 +0930

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+PS3='Please enter your choice or press 3 to quit: '
+options=("Create Storage Management Stack" "Delete Storage Management Stack" "Quit")
+select opt in "${options[@]}"
+do
+  case $opt in
+	"Create Storage Management Stack")
+	  echo "you chose choice 1"
+	  echo '-------------------------------------------------------------'
+	  echo ' 	Please wait 2-5 mins until new prompt message...           '
+	  echo '-------------------------------------------------------------'
+	  aws cloudformation create-stack --stack-name StorageMan --template-body "file://StoreageMan.json" >/dev/null 2>&1
+	  aws cloudformation wait stack-create-complete --stack-name StorageMan
+ 	  echo '-------------------------------------------------------------'
+	  echo '	Setup Completed You can start the assessment tasks          '
+	  echo '-------------------------------------------------------------'
+	  echo "Below is the related information for your reference"
+	  break
+	  ;;
+	"Delete Storage Management Stack")
+	  aws cloudformation delete-stack --stack-name StorageMan
+	  echo '-------------------------------------------------------------'
+	  echo '	Deleting Tag Management Stack may takes 2-5 mins    '
+	  echo '-------------------------------------------------------------'
+	  break
+	  ;;
+	"Quit")
+	  break
+	  ;;
+   	*) echo "invalid option $REPLY";;
+  esac
+done
